706202da9cb32667fbd1ed898eb2630ac6f5202a
Angel Biedma <tekeiro@gmail.com>
2017-11-03 21:05:01 +0100

    Initial structure of Aurora projects and LibGDX

---
+++ test-game-android/src/org/keirobm/aurora/testgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.keirobm.aurora.testgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.keirobm.aurora.testgame.TestGameApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TestGameApp(), config);
+	}
+}
